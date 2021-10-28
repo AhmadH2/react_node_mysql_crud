daca4e01930bc3896c3158e1231a7a05e95b3b3e
Ahmad Horyzat <171059@ppu.edu.ps>
2021-10-28 13:48:59 +0300

    initail commit

---
+++ server/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: 'password',
+  database: 'employee_schema',
+});
+
+app.get('', (req, res) => {
+    db.query(
+        "SELECT * FROM employees",
+        (err,result)=> {
+            if(err) res.send(err)
+            else res.send(result)
+        }
+        )
+})
+
+app.post('/create', (req, res)=> {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+
+    db.query(
+        "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)", 
+        [name, age, country, position, wage],
+        (err, result) => {
+            if(err) {
+                res.send(err)
+            }
+            else {
+                res.send('Values Inserted')
+            }
+    });
+})
+
+app.delete('/:id', (req, res) => {
+    db.query(
+      "DELETE FROM employees WHERE id = (?)",
+      [parseInt(req.params.id)],
+      (err, result) => {
+        if (err) res.send(err);
+        else {
+            db.query("SELECT * FROM employees",
+            (e, r) => res.send(r)
+            )
+        }
+      }
+    );
+})
+
+app.listen(3010, () => {
+    console.log('app is listen to port 3010');
+})
